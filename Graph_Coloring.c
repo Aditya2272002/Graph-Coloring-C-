738300553a52f7275c18414a126aac1da8f2e0a8
Aditya Maurya <61294471+Aditya2272002@users.noreply.github.com>
2021-08-01 15:23:47 +0530

    Update Graph_Coloring.c

--- Graph_Coloring.c
+++ Graph_Coloring.c
@@ -22,13 +22,18 @@ void write(){
 
 void nextColor(int k){
     int i=1;
+   //x[] is array having values from 0-m in index 1-n
+   // i.e which node will be coloured by which color 
+   // x = [ 0th position not filled, 1,0,2]  filling occur from 1st index : node 1 have color 1, node 2 have no color, node 3 have color 2.  
+	
+   // return; means exit suddenly from this function returning void.	
     do{
         x[k] = (x[k]+1)%(m+1); //Highest color that can be used
-        if(x[k]==0)return;
-        for(i=1;i<=n;i++)
+        if(x[k]==0)return; // All colours used then return to function.
+        for(i=1;i<=n;i++)  // Iterating from node 1 to n 
             if(G[i][k]!=0 && x[k]==x[i]) // G[i][k] : means nodes exist between i and k , x[k] == x[i] : means checking ajdacent nodes i and k have same color
                 break;
-        if(i==n+1)
+        if(i==n+1) // if no two adjacent nodes have same color and path exist between them then loop variable will reach to n+1 :- new color found 
             return;
     }while(1);
 }
@@ -36,12 +41,12 @@ void nextColor(int k){
 void mcoloring(int k){
     for(int i=1;i<=n;i++){
         nextColor(k);
-        if(x[k]==0) // if Color can not be filled at kth location
+        if(x[k]==0) // if Color can not be filled at kth location.
             return;
-        if(k==n) // if all nodes are coloured then print
+        if(k==n) // if all nodes are coloured then print.
             write();
         else
-            mcoloring(k+1); // proceed to nexr node
+            mcoloring(k+1); // proceed to next node.
     }
 }
 
